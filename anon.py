86c25deaccda52f3d76c98283e90b24747c8671b
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2013-04-22 11:38:20 +0300

    Collect data nicely

--- anon.py
+++ anon.py
@@ -9,11 +9,26 @@ class Message( chat.Message ):
   def fill(self, msg ):
   	chat.Message.fill( self, msg )
   	self.experiment = msg['_experiment']
+  	self.author_id = msg['from_id']
 
   def __str__(self):
   	s = chat.Message.__str__( self )
-  	return s + self.CVS_SEPARATOR +  str(self.experiment)
+  	experiment = '1' if self.experiment else '0'
+  	return experiment + self.CVS_SEPARATOR + self.author_id
 
 chat.__MESSAGE_CLASS = Message
-chat.analyze_chat('../dev1.db')
-print chat.all_print()
+_authors = {}
+
+def analyze( db ):
+	global _authors
+
+	chat.analyze_chat( db )
+
+	for m in chat.get_messages():
+		u = ( db, m.author_id , m.experiment )
+		if u not in _authors:
+			_authors[ u ] = 0
+		_authors[ u ] += 1
+
+analyze('../dev1.db')
+analyze('../dev2.db')
