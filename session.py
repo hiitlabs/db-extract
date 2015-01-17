2ff5ae0a3f419e2c6ce76bb73c9c0e04caa320c8
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2015-01-17 20:29:42 +0200

    Fix issues on accessing the data from blocks

--- session.py
+++ session.py
@@ -3,6 +3,26 @@ import json
 
 from chat import Message
 
+class _Block:
+
+    def __init__( self, _id, _type, parent ):
+        self.type = _type
+        self.id = _id
+
+        print self.type
+
+        ## collect extra data
+        dbkey = self.type + ':' + self.id
+
+        if self.type == 'chat':
+            dbkey += 'frontends'
+
+        if len( parent.__search_key( dbkey ) ) > 0:
+
+            print parent.__search_key( dbkey )[-1]
+
+
+
 class Block:
 
     def __search_key( self, key ):
@@ -13,7 +33,7 @@ class Block:
             line = json.loads( line )
 
             if line['key'] == key:
-                ret += line['val']
+                ret.append( line['val'] )
 
         return ret
 
@@ -29,28 +49,33 @@ class Block:
         ## manually make sure that we have only one of each
         self.blocks = {}
 
-        for b in blocks:
+        for b in blocks[-1]:
             if b['id'] not in self.blocks:
                 self.blocks[ b['id'] ] = b
+                _Block( b['id'], b['type'], self )
 
         self.blocks = self.blocks.values()
 
         self.content = {}
+
 
         ## check hot to make this smarter
         for block in self.blocks:
 
             if block['type'] == 'chat':
                 key = 'chat:' + block['id'] + 'msgIds'
-                self.content[ block['id'] ] = set( self.__search_key( key ) )
+                self.content[ block['id'] ] = set( self.__search_key( key )[-1] )
 
     def get_blocks( self, type_name, class_name ):
 
         for block in self.blocks:
             if block['type'] == type_name:
-                print map( str, class_name.load_per_block( self.f, self.content[ block['id'] ] ).values() )
+                print block
+                ## all content of this block
+                ## print map( str, class_name.load_per_block( self.f, self.content[ block['id'] ] ).values() )
 
 
 if __name__ == '__main__':
     b = Block( sys.argv[1] )
     b.get_blocks( 'chat' , Message )
+    b.get_blocks( 'scatter', Message )
