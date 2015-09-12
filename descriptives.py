150a6b3e874f71b6da0a4a3b61b036c2fc4ee7fb
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2015-09-12 21:06:27 +0300

    Count activities per block type

--- descriptives.py
+++ descriptives.py
@@ -3,24 +3,43 @@
 import sys
 from session import *
 
+import chat
+import datetime
+
+import collections
+
 def descriptives( file_name ):
 
     session = Presemo( file_name )
 
     print "Number of blocks", len( session.blocks )
 
-    messages = 0
-    votes = 0
+    print "Block types", collections.Counter( map( lambda x: x.type , session.blocks ) )
+
+    messages = collections.defaultdict( int )
+    votes = collections.defaultdict( int )
 
     for block in session.blocks:
 
         activity = block.activity()
 
-        messages += activity[1]
-        votes += activity[2]
+        messages[ block.type ] += activity[1]
+        votes[ block.type ] += activity[2]
 
     print "Total number of messages", messages
     print "Total number of votes", votes
+
+    ## check days from content that has time code
+
+    chat.Message.HEADER = 'thread-message:(.*)' ## fixme
+    messages = chat.Message.load( file_name )
+
+    messages = map( lambda x: x.time, messages.values() )
+    messages = map( lambda x: datetime.datetime.fromtimestamp( x / 1000 ) , messages )
+
+    dates = map( lambda x: x.date() , messages )
+    dates = set( dates )
+    print "Data collected for", len( dates ), "days"
 
 
 if __name__ == '__main__':
