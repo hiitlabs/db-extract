5fe9a328149e8abf911705c9ba24c7dce0e961a6
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2015-09-13 12:18:52 +0300

    Use session-block-content structure instead of hacks

--- timeline.py
+++ timeline.py
@@ -14,7 +14,7 @@ types = {}
 
 #    types[ l[0] ] = l[1]
 
-b = Block( sys.argv[1] + '/objects.dirty' )
+
 
 def out( type, block, content):
 
@@ -25,43 +25,10 @@ def out( type, block, content):
 
     print type , ',', t ,',' , block['id'] , ',' , content.time, content.id, content.text, block['title'].encode('utf8')
 
-## legacy for May 2014 stuff
-# Message.HEADER = 'chat-message:(.*)'
+session = Presemo( sys.argv[1] + '/objects.dirty' )
 
-for block in b.get_blocks( 'chat' , Message ):
+for block in session.blocks:
 
-    for content in block['content']:
+    for content in block.content():
 
-        out( 'chat', block , content )
-
-
-Message.HEADER = 'thread-message:(.*)'
-
-for block in b.get_blocks( 'thread' , Message ):
-
-    for content in block['content']:
-
-        out( 'chat', block , content )
-
-## todo: this should be done with extensions, not hack
-
-Message.HEADER = 'rating:(.*)'
-
-## TODO: fixme
-shown_ids = []
-
-for block in b.get_blocks( 'rating' , Message ):
-
-    for content in block['content']:
-
-        if content.id not in shown_ids: ## todo: fixme
-
-            out( 'vote', block , content )
-
-            shown_ids.append( content.id )
-
-##for block in b.get_blocks( 'poll' , Poll ):
-
-#    for content in block['content']:
-
-#        out( 'poll', block['id'], content )
+        print block.type, block.id, content.time, content.id
