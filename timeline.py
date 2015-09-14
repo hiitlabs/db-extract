acba6c090f7a2a9b76434c9ade32f8af0438d0a1
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2015-09-14 13:15:37 +0300

    Remove type jargon

--- timeline.py
+++ timeline.py
@@ -3,28 +3,6 @@ from chat import *
 from poll import *
 import sys
 
-## initialize content types
-
-types = {}
-
-#f = open('/Users/mnelimar/Desktop/blocks_function.txt')
-
-#for l in f:
-#    l = l.split('|')
-
-#    types[ l[0] ] = l[1]
-
-
-
-def out( type, block, content):
-
-    t = '?????'
-    if id in types:
-
-        t = types[ id ]
-
-    print type , ',', t ,',' , block['id'] , ',' , content.time, content.id, content.text, block['title'].encode('utf8')
-
 session = Presemo( sys.argv[1] + '/objects.dirty' )
 
 for block in session.blocks:
